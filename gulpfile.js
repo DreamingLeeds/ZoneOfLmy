747b0472cf74f903d9df94f28cff26240b098586
DreamingLeeds <1174748035@qq.com>
2016-02-20 17:54:08 +0800

    提交gulp文件

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var minifyCss=require('gulp-minify-css');
+var minifyHtml=require('gulp-minify-html');
+var uglify=require('gulp-uglify');
+var concat=require('gulp-concat');
+
+gulp.task('default',['minifyCss','minifyHtml','uglify']);
+
+gulp.task('minifyHtml',function(){
+    gulp.src('wapdev/*.html')
+        .pipe(minifyHtml())
+        .pipe(gulp.dest('wap'));
+    console.log('html压缩完成');
+});
+
+gulp.task('minifyCss',function(){
+    gulp.src('wapdev/css/*.css')
+        .pipe(minifyCss())
+        .pipe(gulp.dest('wap/css'));
+    console.log('css压缩完成');
+
+});
+
+gulp.task('uglify',function(){
+    gulp.src('wapdev/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('wap/js'));
+	gulp.src('wapdev/js/lib/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('wap/js/lib'));
+    console.log('js压缩完成');
+});
